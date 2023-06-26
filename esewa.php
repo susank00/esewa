fbcb84ba1997a38a12de198cd293273326b4b8bf
susank00 <137717484+susank00@users.noreply.github.com>
2023-06-26 18:30:09 +0545

    Add files via upload

---
+++ esewa.php
@@ -0,0 +1,50 @@
+<?php
+include 'setting.php';
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>purchase ticket managament system</title>
+    <!-- Latest compiled and minified CSS -->
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css">
+</head>
+<body>
+
+<div class="payment-container">
+        <h2>Payment Form</h2>
+        
+    <img src="images/plane.png" alt="plane" class="card-img-top">
+    <div class="card-body">
+        <div class="card-title">
+            purchase ticket
+        </div>
+        <div class="card-text">use this software for purchasing</div>
+        <form action=<?php ?>  method="POST">
+    <input value="100" name="tAmt" type="hidden">
+    <input value="90" name="amt" type="hidden">
+    <input value="5" name="txAmt" type="hidden">
+    <input value="2" name="psc" type="hidden">
+    <input value="3" name="pdc" type="hidden">
+    <input value="EPAYTEST" name="scd" type="hidden">
+    <input value="<?php echo $pid?>" name="pid" type="hidden">
+    <input value="http://merchant.com.np/page/esewa_payment_success?q=su" type="hidden" name="su">
+    <input value="http://merchant.com.np/page/esewa_payment_failed?q=fu" type="hidden" name="fu">
+    <input value="Submit" type="submit" class="btn btn-primary'>
+    </form>
+  
+    </div>
+    </div>
+
+
+<!-- jQuery library -->
+<script src="https://cdn.jsdelivr.net/npm/jquery@3.6.4/dist/jquery.slim.min.js"></script>
+
+<!-- Popper JS -->
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"></script>
+
+<!-- Latest compiled JavaScript -->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js"></script>
+</body>
+</html>
---
+++ failed.php
@@ -0,0 +1,6 @@
+<?php
+echo"<h1> payment failed";
+
+?>
+<a href="https://susank00.github.io/airline-ticket-reservation-system/"> Goto Homepage</a>
+   
